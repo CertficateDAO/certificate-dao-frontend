3a854f5fe30eeae5289385b507d7e219ba379ad9
李树彬 <koreQAQ@foxmail.com>
2021-10-01 23:10:23 +0800

    Feat i18n completed

---
+++ pages/assets/js/i18n.js
@@ -0,0 +1,344 @@
+const messages = {
+    en: {
+        language:"language",
+        purchase:{
+            title:"Purchase Now"
+        },
+        supply:{
+            title:"Live ICO",
+            message:"Total Supply USD $5,000,000"
+        },
+        subject:{
+            news:"News",
+            news_description: "Support news Letter subscription service",
+            title:"The wisdom of group authentication",
+            description:"A knowledge product that is accessible and modifiable to all certified and non-certified users, and on top of this product, " +
+                "a consensus-based, verifiable derivative competency qualification service is provided."
+        },
+        video:{
+            title:"CertificateDAO decentralized qualification",
+            subTitle:"Decentralization of educational resources, to achieve universal benefit model",
+            desc:"Our ability to help others, our ability to treat ourselves, our ability to give back to society"
+        },
+        difference:{
+            title:"What makes it different?",
+            first:{
+                title:"Decentralized educational resources",
+                desc:"CertificateDAO has established a complete convergence of educational resources on relevant capabilities through decentralized advantages. Adopt community self-management mode, give full play to the wisdom of collective cognition.",
+
+            },
+            second:{
+                title:"Consensus competency authentication mechanism",
+                desc:"CertificateDAO uses a free, mutual-knowledge, game-theory-based equilibrium approach to make certification work consensus-based. At the same time, the certificate of CertificateDAO is a kind of NFT, which makes the proof of knowledge and ability transparent and visual.",
+
+            },
+            third:{
+                title:"Token incentive Behavior Mechanism: SOR model",
+                desc:"CertificateDAO's CFE coins focus more on tools and rewards than traditional Token money mechanisms. This also makes CertificateDAO a PoA mechanism to some extent.",
+
+            },
+            more:"Learn More"
+        },
+        team:{
+            title:"Meet the experts",
+            CEO:{
+                title:"CEO",
+                name:"Shubin Li"
+            },
+            CTO: {
+                title: "CTO",
+                name: "Shubin Li"
+            },
+            advisor_first:{
+                title: "Advisor",
+                name: "Shubin Li"
+            },
+            advisor_second:{
+                title: "Advisor",
+                name: "Shubin Li"
+            },
+            advisor_third:{
+                title: "Advisor",
+                name: "Shubin Li"
+            },
+            advisor_fourth:{
+                title: "Advisor",
+                name: "Shubin Li"
+            },
+
+        },
+        whitePaper:{
+            title:"Get the White Paper",
+            desc:"CFE: a decentralized accrediting agency for educational resources and competencies",
+            subTitle:"A knowledge product that is available and modifiable to all certified and non-certified users, and on top of that product, a Nash equilibrium based, verifiable derivative competency qualification service is provided." ,
+            download:"Download White Paper"
+        },
+        explore: {
+            title:"Explore",
+            course: 'Course',
+            book:"Book",
+            ebook:"Resources",
+            history_news:"Event"
+        },
+        nantan: {
+            title:"NanTan",
+            about:"About",
+            action:"Action",
+            spirit:"Spirit",
+            notebook:"NoteBook"
+        },
+        certificate:{
+            title:"Certificate",
+            description:"Description",
+            how_to:"How-To"
+        },
+        scope:{
+            title:"Scope",
+            defi:"Decentralized finance",
+            nft:"Non-fungible token",
+            metaverse:"Metaverse"
+        },
+        discuss:{
+            title:"Discuss",
+        },
+        help:{
+            title:"Help Support",
+            product:"Product Support",
+            doc:{
+                title:"Docs",
+                message:"Get all the information you need"
+            },
+            manual:{
+                title:"Looking for answers?",
+                message:"Get support"
+            }
+        },
+
+        subscribe:{
+            title:"We’ll let you know the moment we launch.",
+            notified:"Get Notified",
+            thanks:"Thanks, a member of our team will be in touch shortly.",
+            error:"Please fill all fields correctly.Or email has subscribed",
+            policy:{
+                message:"We’ll never share your details with third parties.",
+                action:"View our",
+                title:"Private Policy",
+                href:"Privacy Policy",
+                more:"for more info."
+            }
+        },
+        roadmap:{
+            title:"RoadMap",
+            desc:"All great things come from perseverance",
+            first:{
+                time:"October 2021",
+                desc:"CertificateDAO project start"
+            },
+            second:{
+                time:"November 2021",
+                desc:"Project core resources research and development, infrastructure construction"
+            },
+            third:{
+                time:"December 2021",
+                desc:"V1.0 RELEASE Version is released"
+            },
+            forth:{
+                time:"January 2022",
+                desc:"Kovan test network start"
+            },
+            five:{
+                time:"February 2022",
+                desc:"ETH main network start"
+            },
+        },
+        foot:{
+            copyright:"Copyright 2005 CertificateDAO® is a registered trademark of your company. " +
+                "The CertificateDAO® logo is a registered trademark of CertificateDAO Corporation in the United States and other countries."
+        }
+
+    },
+    zh: {
+        language:"语言",
+        subject:{
+            news:"最新进展",
+            news_description:"支持news letter 订阅服务",
+            title:"群体认证的智慧",
+            description:"让所有认证、非认证的用户都可以使用并且可以进行修改的知识产品，并在此产品基础上提供基于共识机制的、可验证的衍生能力资格认证服务。"
+       },
+        purchase:{
+            title:"现在购买"
+        },
+        supply:{
+            title:"ICO 进行中",
+          message:"初始总量价值 USD $5,000,000"
+        },
+        video:{
+            title:"CertificateDAO 去中心化资格认证",
+            subTitle:"教育资源的去中心化，实现普惠模式",
+            desc:"我们的能力可以帮助他人，我们的能力可以善待自己，我们的能力可以回报社会"
+        },
+        difference:{
+            title:"CertificateDAO 有何优势 ?",
+            first:{
+                title:"去中心化的教育资源",
+                desc:"CertificateDAO 通过去中心化优势，建立了关于相关能力完整的教育资源融合。采用社区自营模式，充分发挥集体认知的智慧。",
+
+            },
+            second:{
+                title:"共识能力认证机制",
+                desc:"CertificateDAO 采用免费的、社区成员共同认知，且基于博弈论的均衡思想来使得认证工作建立在共识之上；\n" +
+                    " 同时，CertificateDAO 的认证凭证是一种 NFT ，这一特殊性质使得知识、能力证明变得透明、可视化。",
+
+            },
+            third:{
+                title:"代币刺激行为机制：SOR模型",
+                desc:"相比于传统Token（代币机制）的货币机制，CertificateDAO 的 CFE 代币更多地侧重于工具、奖励机制。\n" +
+                "这也使得CertificateDAO 在一定程度上是一种 PoA 机制。",
+
+            },
+            more:"了解更多"
+        },
+        team:{
+            title:"项目团队",
+            CEO:{
+                title:"首席执行官",
+                name:"李树彬"
+            },
+            CTO: {
+                title: "技术总监",
+                name: "李树彬"
+            },
+            advisor_first:{
+                title: "顾问",
+                name: "李树彬"
+            },
+            advisor_second:{
+                title: "顾问",
+                name: "李树彬"
+            },
+            advisor_third:{
+                title: "顾问",
+                name: "李树彬"
+            },
+            advisor_fourth:{
+                title: "顾问",
+                name: "李树彬"
+            },
+
+        },
+        whitePaper:{
+            title:"项目白皮书",
+            desc:"去中心化的教育资源与能力认证机构",
+            subTitle:"让所有认证、非认证的用户都可以使用并且可以进行修改的知识产品，并在此产品基础上提供基于纳什均衡的、可验证的衍生能力资格认证服务。" ,
+            download:"下载白皮书"
+        },
+        explore: {
+            title: "探索",
+            course: '课程',
+            book:"实体书",
+            ebook:"电子教材",
+            history_news:"历史资讯"
+        },
+        nantan: {
+            title:"南坦",
+            about:"关于南坦",
+            action:"行动指引",
+            spirit:"行动精神",
+            notebook:"指引手册"
+        },
+        certificate:{
+            title:"资格认证",
+            description:"认证说明",
+            how_to:"如何参与"
+        },
+        scope:{
+            title:"领域",
+            defi:"去中心化",
+            nft:"非同质化代币",
+            metaverse:"元宇宙"
+        },
+        discuss:{
+            title:"讨论",
+        },
+        help:{
+            title:"帮助与支持",
+            product:"产品支持",
+            doc:{
+                title:"产品文档",
+                message:"获取想要的帮助信息"
+            },
+            manual:{
+                title:"寻求帮助?",
+                message:"人工支持"
+            }
+        },
+        subscribe:{
+            title:"我们会在第一时间通知你.",
+            notified:"订阅",
+            thanks:"谢谢，我们团队的一位成员将很快与您联系。",
+            error:"请正确填写相关信息；或者您已经订阅了此服务，请查阅邮件",
+            policy:{
+                message:"我们不会把你的隐私透露给第三方。",
+                action:"查阅",
+                title:"隐私协议",
+                more:"获取更多信息。"
+            }
+        },
+        roadmap:{
+            title:"路线图",
+            desc:"任何伟大的事情都源自于坚持不懈",
+            first:{
+                time:"2021 10",
+                desc:"CertificateDAO 立项"
+            },
+            second:{
+                time:"2021 11",
+                desc:"项目核心资源研发、基础设施搭建"
+            },
+            third:{
+                time:"2021 12",
+                desc:"v1.0 RELEASE 版本发布"
+            },
+            forth:{
+                time:"2022 1",
+                desc:"Kovan 测试网络上线"
+            },
+            five:{
+                time:"2022 2",
+                desc:"ETH 主网上线"
+            },
+        },
+        foot:{
+            copyright:"版权所有CertificateDAO®是贵公司的注册商标。\n " +
+                "CertificateDAO®标识是CertificateDAO公司在美国和其他国家注册的商标。"
+        }
+
+
+
+    }
+}
+// 2. Create i18n instance with options
+const i18n = VueI18n.createI18n({
+    locale: 'en', // set locale
+    fallbackLocale: 'zh', // set fallback locale
+    messages, // set locale messages
+    // If you need to specify other options, you can set other options
+    // ...
+})
+// 3. Create a vue root instance
+const app = Vue.createApp({
+    // set something options
+    // ...
+})
+
+// 4. Install i18n instance to make the whole app i18n-aware
+app.use(i18n)
+// 5. Mount
+app.mount('#index')
+// Now the app has started!
+
+function changeLocale(locale){
+    console.log("origin locale"+ i18n.global.locale)
+    i18n.global.locale = locale
+    console.log("now locale"+ i18n.global.locale)
+}
